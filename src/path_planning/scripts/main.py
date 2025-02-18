f7d84e434f08d710aa683d278a534e92fe1090de
federicolozer <lozer@VNB217.technikum.local>
2025-02-18 09:58:28 +0100

    18/02/25 - passed multiple responses in socket communication

--- src/path_planning/scripts/main.py
+++ src/path_planning/scripts/main.py
@@ -14,21 +14,42 @@ import time
 import socket
 import struct
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 
 
 def IK_fromQuater_client(data):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8081))
 
     print("data = ", data)
     data = np.array(data, dtype=np.double)
 
     request = data.tobytes()
 
-    client_socket.connect(('localhost', 8081))
+    
     client_socket.sendall(request)
      
-    response = np.frombuffer(client_socket.recv(1024), dtype=np.double)
-    
+    res1 = np.frombuffer(client_socket.recv(56), dtype=np.double)
+    res2 = np.frombuffer(client_socket.recv(56), dtype=np.double)
+    res3 = np.frombuffer(client_socket.recv(56), dtype=np.double)
+    res4 = np.frombuffer(client_socket.recv(56), dtype=np.double)
+
+    print("res1 = ", res1)
+    print("res2 = ", res2)
+    print("res3 = ", res3)
+    print("res4 = ", res4)
+
+    response = []
+
+    if np.isnan(res1).any() == False:
+        response.append(res1)
+    elif np.isnan(res2).any() == False:
+        response.append(res2)
+    elif np.isnan(res3).any() == False:
+        response.append(res3)
+    elif np.isnan(res4).any() == False:
+        response.append(res4)
+        
     client_socket.close()
 
     return response
@@ -43,59 +64,67 @@ if __name__ == '__main__':
 
     ttype = "follow_joint"
 
-    with open('/home/lozer/franka_emika_ws/src/neural_network/data/dataset/humanPoses.csv') as file:
-        reader = csv.reader(file)
+    while True:
+        with open('/home/lozer/franka_emika_ws/src/neural_network/data/dataset/humanPoses.csv') as file:
+            reader = csv.reader(file)
 
-        row = list(reader)[200]
+            rw = input("Select row...\n")
+            if rw == "stop":
+                break
 
-        t = []
-        q = []
+            row = list(reader)[int(rw)]
 
-        #quater = np.array([float(row[0]), float(row[1]), float(row[2]), float(row[3])])
-        #O_EE = np.array([float(row[4]), float(row[5]), float(row[6])])
-#
-        #t_1 = time.time()
-        #
-        #model = nn.NN()
-        #t0 = time.time()
-        #print("time elapsed for initialize NN: ", t0-t_1, "s")
-#
-        #inputData = np.matrix(np.concatenate((quater, O_EE), axis=0))
-        #q7_tmp = nn.neural_network(model, inputData)
-#
-        #print("\n----------------")
-        #t1 = time.time()
-        #print("time elapsed for having a solution: ", t1-t0, "s")
+            t = []
+            q = []
+
+            #quater = np.array([float(row[0]), float(row[1]), float(row[2]), float(row[3])])
+            #O_EE = np.array([float(row[4]), float(row[5]), float(row[6])])
     #
-        print(row)
+            #t_1 = time.time()
+            #
+            #model = nn.NN()
+            #t0 = time.time()
+            #print("time elapsed for initialize NN: ", t0-t_1, "s")
+    #
+            #inputData = np.matrix(np.concatenate((quater, O_EE), axis=0))
+            #q7_tmp = nn.neural_network(model, inputData)
+    #
+            #print("\n----------------")
+            #t1 = time.time()
+            #print("time elapsed for having a solution: ", t1-t0, "s")
+        #
+            print(row)
 
-        t2 = time.time()
+            t2 = time.time()
 
-        #res = controller.IK_fromQuater_client(quater, O_EE, q7, q_actual_array, horz)
+            #res = controller.IK_fromQuater_client(quater, O_EE, q7, q_actual_array, horz)
 
-        data = [float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6]), pi/4-float(row[7]), int(horz)]
-        response = IK_fromQuater_client(data)
+            data = [float(row[0]), float(row[1]), float(row[2]), float(row[3]), float(row[4]), float(row[5]), float(row[6]), pi/4-float(row[7]), int(horz)]
+            response = IK_fromQuater_client(data)
 
-        print("\n----------------")
-        t3 = time.time()
-        print("time elapsed for IK: ", t3-t2, "s\n")
+            print("\n----------------")
+            t3 = time.time()
+            print("time elapsed for IK: ", t3-t2, "s\n")
 
-        print("res = ", response)
+            print("----- response = ", response)
+            
+            if response == []:
+                print("No response found")
+                quit()
+
+            q_array = list(response[0])
+            #q_array = [-1.7623606392141309, 1.0066890508374107, 0.9714105572606898, -1.9972501746915912, -0.5232118417432983, 2.2464632219400054, -0.40214716339744827]
+            print("q_array = ", q_array)
+            print("len q_array = ", len(q_array))
+
+            if not len(q_array) == 0:
+                t.append(2)
+                q.append(q_array)
+
+                controller.launch_trajectory(t, q, ttype)
+
+                time.sleep(5)
         
-
-
-        q_array = list(response)
-        #q_array = [-1.7623606392141309, 1.0066890508374107, 0.9714105572606898, -1.9972501746915912, -0.5232118417432983, 2.2464632219400054, -0.40214716339744827]
-        print("q_array = ", q_array)
-        print("len q_array = ", len(q_array))
-
-        if not len(q_array) == 0:
-            t.append(2)
-            q.append(q_array)
-
-            controller.launch_trajectory(t, q, ttype)
-
-            time.sleep(5)
     
     
 
--- src/path_planning/src/IK_server.cpp
+++ src/path_planning/src/IK_server.cpp
@@ -31,8 +31,8 @@ void printFrame(Eigen::Matrix4d O_T_EE_tmp) {
 boost::array<boost::array<double, 7>, 4> IK_fromQuater(Eigen::Quaterniond quater, std::array<double, 3> O_EE, double q7, bool horz) {  
     std::chrono::time_point<std::chrono::system_clock> t_start = std::chrono::system_clock::now();
     
-    std::cout << "quater = " << quater.x() << " " << quater.y() << " " << quater.z() << " " << quater.w() << " " << std::endl;
-    std::cout << "O_EE = " << O_EE[0] << " " << O_EE[1] << " " << O_EE[2] << " " << std::endl;
+    //std::cout << "quater = " << quater.x() << " " << quater.y() << " " << quater.z() << " " << quater.w() << " " << std::endl;
+    //std::cout << "O_EE = " << O_EE[0] << " " << O_EE[1] << " " << O_EE[2] << " " << std::endl;
 
     Eigen::Matrix4d O_T_EE_mat = quaternionToFrame(quater, O_EE[0], O_EE[1], O_EE[2]);
 
@@ -42,22 +42,22 @@ boost::array<boost::array<double, 7>, 4> IK_fromQuater(Eigen::Quaterniond quater
 
     if (horz) {
         Eigen::Matrix4d O_T_EE_rot(O_T_EE_mat.data());
-        std::cout << "O_T_EE = " << std::endl << O_T_EE_rot << std::endl;
+        //std::cout << "O_T_EE = " << std::endl << O_T_EE_rot << std::endl;
         Eigen::Matrix4d baseToWall_rot;
         baseToWall_rot << 0.0, 0.0, 1.0, -0.333, 
                             0.0, 1.0, 0.0, 0.0, 
                             -1.0, 0.0, 0.0, 0.7, 
                             0.0, 0.0, 0.0, 1.0;
-        std::cout << "baseToWall_rot = " << std::endl << baseToWall_rot << std::endl;
+        //std::cout << "baseToWall_rot = " << std::endl << baseToWall_rot << std::endl;
         O_T_EE_tmp = baseToWall_rot.inverse()*O_T_EE_rot;
-        std::cout << "dot product = " << std::endl << O_T_EE_tmp << std::endl;
+        //std::cout << "dot product = " << std::endl << O_T_EE_tmp << std::endl;
     }
     else {
         O_T_EE_tmp = Eigen::Matrix4d(O_T_EE_mat.data());
     }
     Eigen::Map< Eigen::Matrix4d > O_T_EE(O_T_EE_tmp.data());
 
-    std::cout << "O_T_EE = " << std::endl << O_T_EE << std::endl;
+    //std::cout << "O_T_EE = " << std::endl << O_T_EE << std::endl;
 
     boost::array<boost::array<double, 7>, 4> q_array_list = franka_IK(O_T_EE, q7, q_actual_array);
 
@@ -81,7 +81,8 @@ void server() {
 
     bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress));
 
-    while (true) {
+    bool a = true;
+    while (a == true) {
         listen(serverSocket, 5);
         int new_socket = accept(serverSocket, nullptr, nullptr);
 
@@ -91,53 +92,26 @@ void server() {
         double* buffer = new double[9];
         read(new_socket, buffer, 72);
 
-        std::cout << "buffer = " << buffer[0] << " " << buffer[1] << " " << buffer[2] << " " << buffer[3] << " " << buffer[4] << " " << buffer[5] << " " << buffer[6] << " " << buffer[7] << " " << buffer[8] << " " << std::endl;
-
         std::array<double, 4> quaternion;
         for (int i=0; i<4; i++) {
             quaternion[i] = buffer[i];
         }
         Eigen::Quaterniond quater(quaternion.data());
 
-        std::cout << "quaternion = " << quater.x() << " " << quater.y() << " " << quater.z() << " " << quater.w() << " " << std::endl;
-
         std::array<double, 3> O_EE;
         for (int i=0; i<3; i++) {
             O_EE[i] = buffer[i+4];
         }
-
-        std::cout << "O_EE = " << O_EE[0] << " " << O_EE[1] << " " << O_EE[2] << " " << std::endl;
-        
+  
         double q7 = buffer[7];
-
         bool horz = buffer[8];
 
         boost::array<boost::array<double, 7>, 4> q_array_list = IK_fromQuater(quater, O_EE, q7, horz);
 
-        std::vector<boost::array<double, 7>> q_array;
-
-        for (int i=0; i<4; i++) {
-            bool corr = true;
-            for (auto arr=q_array_list[i].begin(); arr<q_array_list[i].end(); arr++) {
-                if (std::isnan(*arr)) {
-                    corr = false;
-                    break;
-                }
-            }
-
-            if (corr == true) {
-                q_array.push_back(q_array_list[i]);                
-            }
-        }
-
-        //std::cout << "q array = " << q_array[0][0] << " " << q_array_list[0][1] << " " << q_array_list[0][2] << " " << q_array_list[0][3] << " " << q_array_list[0][4] << std::endl;
-
-        if (q_array.size() >= 1) {
-            send(new_socket, &q_array[0], sizeof(q_array[0]), 0);
-        }
-        else {
-            send(new_socket, 0, 1, 0);
-        }
+        send(new_socket, &q_array_list[0], sizeof(q_array_list[0]), 0);
+        send(new_socket, &q_array_list[1], sizeof(q_array_list[1]), 0);
+        send(new_socket, &q_array_list[2], sizeof(q_array_list[2]), 0);
+        send(new_socket, &q_array_list[3], sizeof(q_array_list[3]), 0);
         
         close(new_socket);
 
