1424b7cf088bb41f1916b675c034bdedb216dde3
federicolozer <lozer@VNB217.technikum.local>
2025-02-20 18:23:05 +0100

    20/02/25 - fixed gravity compensation on gazebo

--- src/path_planning/scripts/change_gravity.py
+++ src/path_planning/scripts/change_gravity.py
@@ -11,9 +11,7 @@ from std_srvs.srv import Empty
 
 if __name__ == '__main__':    
     rospy.init_node('change_gravity')
-    set_physics = rospy.ServiceProxy("/gazebo/set_physics_properties", SetPhysicsProperties)
-    unpause_physics = rospy.ServiceProxy("/gazebo/unpause_physics", Empty)
-
+    
     gravity = Vector3()
     mode = 0
     if not rospy.search_param('/mode') == None:
@@ -28,12 +26,5 @@ if __name__ == '__main__':
             mode = 3
             gravity.z = 9.8
 
-    print("########################################")
-    print(set_physics.call(gravity=gravity))
-    print("########################################")
     
-    unpause_physics.call()
-
-    reset = rospy.ServiceProxy("/gazebo/reset_world", Empty)
-    print(reset.call())
    
--- src/path_planning/scripts/main.py
+++ src/path_planning/scripts/main.py
@@ -13,9 +13,10 @@ import csv
 import time
 import socket
 import struct
-from gazebo_msgs.srv import SetPhysicsProperties, GetPhysicsProperties
-from geometry_msgs.msg import Vector3
-from std_srvs.srv import Empty
+#from gazebo_msgs.srv import SetPhysicsProperties, GetPhysicsProperties
+#from geometry_msgs.msg import Vector3
+#from std_srvs.srv import Empty
+import os
 
 
 
@@ -99,19 +100,31 @@ if __name__ == '__main__':
     #set_physics = rospy.ServiceProxy("/gazebo/set_physics_properties", SetPhysicsProperties)
     #get_physics = rospy.ServiceProxy("/gazebo/get_physics_properties", GetPhysicsProperties)
 
-    gravity = Vector3()
+    #gravity = Vector3()
     mode = 0
+    gravity = [0, 0, 0]
     if not rospy.search_param('/mode') == None:
         param = rospy.get_param('/mode')
         if param == "horz":
             mode = 1
-            gravity.x = -9.8
+            gravity[0] = -9.8
         elif param == "horz_rev":
             mode = 2
-            gravity.x = -9.8
+            gravity[0] = -9.8
         elif param == "ceil":
             mode = 3
-            gravity.z = 9.8
+            gravity[2] = 9.8
+        else:
+            gravity[2] = -9.8
+            
+    print(gravity)
+    print(mode)
+
+    msg = "rosrun dynamic_reconfigure dynparam set /gazebo \"{" + f"'gravity_x':{gravity[0]}, 'gravity_y':{gravity[1]}, 'gravity_z':{gravity[2]}" + "}\""
+    print(msg)
+    os.system(msg)
+
+    quit()
 
     #print(set_physics.call(gravity=gravity))
     #print(get_physics.call())
