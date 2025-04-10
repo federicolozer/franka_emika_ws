8bbcf9815fa4d981b02616925f448c1f4b3dcd33
federicolozer <lozer@VNB217.technikum.local>
2025-04-10 16:51:37 +0200

    10/04/25 - added new tests, not working so fine

--- src/path_planning/scripts/main.py
+++ src/path_planning/scripts/main.py
@@ -136,7 +136,7 @@ def optMove(q_array_list, q_actual_array):
     
     if not q_array_list == []:
         for array in q_array_list:
-            n_err = np.dot((array-q_actual_array), (array-q_actual_array))
+            n_err = np.dot((array[2]-q_actual_array[2]), (array[2]-q_actual_array[2]))
 
             if n_err-err < 0 or np.isnan(n_err-err):
                 q_array = list(array)
@@ -164,11 +164,14 @@ def sel_mode():
 
 
 if __name__ == '__main__':
-    #rospy.init_node("main")
+    rospy.init_node("main")
 
     mode = sel_mode()
 
     model = nn.createModel()
+
+    print("\n===============================================================")
+    input("Press any key to start\n")
 
     while True:
         t_arm = []
@@ -185,120 +188,118 @@ if __name__ == '__main__':
             pass
 
         print("\n===============================================================")
-        print("Select trajectory to perform or type quit to exit:\n")
+        print("Select the trajectory to perform or quit to exit:\n")
         
         traj = UI_server()
         print(traj)
 
         if traj == "quit":
-            endTransmission(8080)
-            endTransmission(8081)
-            endTransmission(8082)
+            #endTransmission(8080)
             break
         
-        try:
-            with open(f'/home/lozer/franka_emika_ws/src/neural_network/data/dataset/{traj}.csv') as file:
-                doOnce = True
-                for line in csv.reader(file):
-                    if doOnce:
-                        doOnce = False
-                        continue
+        #try:
+        with open(f'/home/lozer/franka_emika_ws/src/neural_network/data/dataset/{traj}.csv') as file:
+            doOnce = True
+            for line in csv.reader(file):
+                if doOnce:
+                    doOnce = False
+                    continue
 
-                    q7_real_array.append(float(line[7]))
-                
-            with open(f'/home/lozer/franka_emika_ws/src/path_planning/data/trajectory/{traj}/gripper.json', "r") as file:
-                trajectory = json.load(file)
-
-                for waypoint in trajectory["waypoints"]:
-                    if waypoint["action"] == "close":
-                        q_array = 0
-                    elif waypoint["action"] == "open":
-                        q_array = 1
-                    t_gripper.append(waypoint["t"]*2)
-                    q_gripper.append(q_array)
+                q7_real_array.append(float(line[7]))
             
-            with open(f'/home/lozer/franka_emika_ws/src/path_planning/data/trajectory/{traj}/arm.json', "r") as file:
-                trajectory = json.load(file)
+        with open(f'/home/lozer/franka_emika_ws/src/path_planning/data/trajectory/{traj}/gripper.json', "r") as file:
+            trajectory = json.load(file)
 
-                t_array = []
+            for waypoint in trajectory["waypoints"]:
+                if waypoint["action"] == "close":
+                    q_array = 0
+                elif waypoint["action"] == "open":
+                    q_array = 1
+                t_gripper.append(waypoint["t"]*2)
+                q_gripper.append(q_array)
+        
+        with open(f'/home/lozer/franka_emika_ws/src/path_planning/data/trajectory/{traj}/arm.json', "r") as file:
+            trajectory = json.load(file)
 
-                # Neural network ---------------------------------------------------------------------
+            t_array = []
 
-                print("\n===============================================================")
-                print("\tNeural network")
-                print("===============================================================")
-                t0 = time.time()
-                for waypoint in trajectory["waypoints"]:
-                    quater = np.array([float(waypoint["Qx"]), float(waypoint["Qy"]), float(waypoint["Qz"]), float(waypoint["Qw"])])
-                    O_EE = np.array([float(waypoint["x"]), float(waypoint["y"]), float(waypoint["z"])])
+            # Neural network ---------------------------------------------------------------------
 
-                    inputData = np.matrix(np.concatenate((quater, O_EE), axis=0))
-                    q7 = float(nn.neuralNetwork(model, inputData)[0]) 
+            print("\n===============================================================")
+            print("\tNeural network")
+            print("===============================================================")
+            t0 = time.time()
+            for waypoint in trajectory["waypoints"]:
+                quater = np.array([float(waypoint["Qx"]), float(waypoint["Qy"]), float(waypoint["Qz"]), float(waypoint["Qw"])])
+                O_EE = np.array([float(waypoint["x"]), float(waypoint["y"]), float(waypoint["z"])])
 
-                    inputData_array.append(deepcopy(inputData))
-                    q7_array.append(deepcopy(q7))
+                inputData = np.matrix(np.concatenate((quater, O_EE), axis=0))
+                q7 = float(nn.neuralNetwork(model, inputData)[0]) 
 
-                    t_array.append(waypoint["t"])
+                inputData_array.append(deepcopy(inputData))
+                q7_array.append(deepcopy(q7))
 
-                tn = time.time()
-                print(f"Elapsed time for having a solution from NN: {(tn-t0):>4f} s")
-                print("---------------------------------------------------------------")
+                t_array.append(waypoint["t"])
 
-                # Savitzky-Golay filter -----------------------------------------------------------------
+            tn = time.time()
+            print(f"Elapsed time for having a solution from NN: {(tn-t0):>4f} s")
+            print("---------------------------------------------------------------")
 
-                q7_array = savgol_filter(q7_array, window_length=int(0.15*len(q7_array)), polyorder=3)
+            # Savitzky-Golay filter -----------------------------------------------------------------
 
-                # Inverse kinematics -----------------------------------------------------------------
+            q7_array = savgol_filter(q7_array, window_length=int(0.15*len(q7_array)), polyorder=3)
 
-                print("\n===============================================================")
-                print("\tInverse kinematics")
-                print("===============================================================")
-                t0 = time.time()
-                cnt = 0
-                for i in range(len(inputData_array)):
-                    inputData = inputData_array[i]
-                    q7 = q7_array[i]
-                    
-                    data = [float(inputData[0, 0]), float(inputData[0, 1]), float(inputData[0, 2]), float(inputData[0, 3]), float(inputData[0, 4]), float(inputData[0, 5]), float(inputData[0, 6]), q7, float(mode), float(dispFrame)]
-                    response = IK_fromQuater_client(data)
+            # Inverse kinematics -----------------------------------------------------------------
 
-                    q_array = optMove(response, q_actual_array)
-
-                    if not len(q_array) == 0:
-                        t_arm.append(t_array[i]*2)
-                        q_arm.append(q_array)
-                        q_actual_array = q_array
-                        cnt += 1
-                    else:
-                        pass
-                        #t.append(None)     iot ce fa di chescj
-                        #q.append(None)
+            print("\n===============================================================")
+            print("\tInverse kinematics")
+            print("===============================================================")
+            t0 = time.time()
+            cnt = 0
+            for i in range(len(inputData_array)):
+                inputData = inputData_array[i]
+                q7 = q7_array[i]
                 
-                tn = time.time()
-                print(f"Elapsed time for having a solution from IK client: {(tn-t0):>4f} s")
-                print("---------------------------------------------------------------")
+                data = [float(inputData[0, 0]), float(inputData[0, 1]), float(inputData[0, 2]), float(inputData[0, 3]), float(inputData[0, 4]), float(inputData[0, 5]), float(inputData[0, 6]), q7, float(mode), float(dispFrame)]
+                response = IK_fromQuater_client(data)
 
-                # Test evaluation ----------------------------------------------------------------
+                q_array = optMove(response, q_actual_array)
 
-                diff = 0
-                for i in range(len(q7_array)):
-                    comp = (q7_array[i]-q7_real_array[i])**2
-                    diff += comp
+                if not len(q_array) == 0:
+                    t_arm.append(t_array[i]*2)
+                    q_arm.append(q_array)
+                    q_actual_array = q_array
+                    cnt += 1
+                else:
+                    pass
+                    #t.append(None)     iot ce fa di chescj
+                    #q.append(None)
+            
+            tn = time.time()
+            print(f"Elapsed time for having a solution from IK client: {(tn-t0):>4f} s")
+            print("---------------------------------------------------------------")
 
-                diff /= len(q7_array)
-                rmse = np.sqrt(diff)
+            # Test evaluation ----------------------------------------------------------------
 
-                print(f"RMSE: {(rmse):>0.4f}rad - {(rmse/(2*2.8973)*100):>0.1f}%")
+            diff = 0
+            for i in range(len(q7_array)):
+                comp = (q7_array[i]-q7_real_array[i])**2
+                diff += comp
 
-                # Trajectory planning ----------------------------------------------------------------
+            diff /= len(q7_array)
+            rmse = np.sqrt(diff)
 
-                print("\n===============================================================")
-                print("\tTrajectory planning")
-                print("===============================================================")
+            print(f"RMSE: {(rmse):>0.4f}rad - {(rmse/(2*2.8973)*100):>0.1f}%")
 
-                print(f"Solutions found: {cnt}/{len(trajectory['waypoints'])}")
+            # Trajectory planning ----------------------------------------------------------------
 
-                #controller_client((t_arm, q_arm, t_gripper, q_gripper, ttype))
-                controller.launch_trajectory(t_arm, q_arm, t_gripper, q_gripper, ttype)
-        except:
-            print("Selected trajectory does not exist..")
+            print("\n===============================================================")
+            print("\tTrajectory planning")
+            print("===============================================================")
+
+            print(f"Solutions found: {cnt}/{len(trajectory['waypoints'])}")
+
+            #controller_client((t_arm, q_arm, t_gripper, q_gripper, ttype))
+            controller.launch_trajectory(t_arm, q_arm, t_gripper, q_gripper, ttype)
+        #except:
+        #    print("Selected trajectory does not exist..")
